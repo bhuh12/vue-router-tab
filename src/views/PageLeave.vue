5d57e60e50017a913fd3b7c3950f7c4160c40492
bhuh12 <bhuh12@126.com>
2020-07-23 09:50:28 +0800

    feat: beforePageLeave 支持浏览器 beforeunload 事件
    BREAKING CHANGE:
    beforePageLeave 弃用参数 resolve, reject，返回 promise 来允许或者阻止页签页面的离开

--- src/views/PageLeave.vue
+++ src/views/PageLeave.vue
@@ -13,25 +13,27 @@
 
     <p>
       <a class="demo-btn" @click="$routerTab.refresh(null, true, false)">
-        刷新
+        刷新页签
       </a>
 
-      <a class="demo-btn" @click="$routerTab.close({ force: false })">关闭</a>
+      <a class="demo-btn" @click="$routerTab.close({ force: false })">
+        关闭页签
+      </a>
 
       <router-link
         class="demo-btn"
         :to="`?id=${+(this.$route.query.id || 0) + 1}`"
       >
-        替换
+        替换页签
       </router-link>
 
-      <router-link
-        class="demo-btn"
-        title="离开页签所在路由"
-        to="/default/page/1"
-      >
-        离开
+      <router-link class="demo-btn" to="/default/page/1">
+        离开路由
       </router-link>
+
+      <a class="demo-btn" @click="reload">
+        刷新浏览器
+      </a>
     </p>
   </div>
 </template>
@@ -51,8 +53,26 @@ export default {
     }
   },
 
-  // 页面离开前确认
-  beforePageLeave(resolve, reject, tab, type) {
+  /**
+   * 页面离开前确认
+   * @param {Object} tab 页签信息
+   * @param {String} type 离开类型：
+   *   close: 页签关闭
+   *   refresh: 页签刷新
+   *   replace: 页签被替换
+   *   leave: 路由离开
+   *   unload: 浏览器刷新或者关闭
+   * @returns {String|Promise}
+   */
+  beforePageLeave(tab, type) {
+    // 值未改变，则直接离开页签
+    if (this.editValue === this.value) return
+
+    // 浏览器窗口刷新或者关闭时，支持的浏览器会展示确认消息
+    if (type === 'unload') {
+      return `您在“${tab.title}”页签的更改尚未完成，是否要离开？`
+    }
+
     // 离开类型
     const action = {
       close: '关闭',
@@ -63,26 +83,26 @@ export default {
 
     const msg = `您确认要${action}页签“${tab.title}”吗？`
 
-    // 值未改变，则直接离开页签
-    if (this.editValue === this.value) {
-      resolve()
-      return
-    }
+    // 返回 promise，resolve 离开，reject 阻止离开
+    return new Promise((resolve, reject) => {
+      // 值改变则确认提示
+      if (confirm(msg)) {
+        resolve()
+      } else {
+        reject(`您拒绝了${action}页签`)
+      }
+    })
 
-    // 值改变则确认提示
-    if (confirm(msg)) {
-      resolve()
-    } else {
-      reject('拒绝了页面离开')
-    }
-
-    /*
     // 此处使用了 Element 的 confirm 组件
     // 需将 closeOnHashChange 配置为 false，以避免路由切换导致确认框关闭
-    this.$confirm(msg, '提示', { closeOnHashChange: false })
-      .then(resolve)
-      .catch(reject)
-    */
+    // return this.$confirm(msg, '提示', { closeOnHashChange: false })
+  },
+
+  methods: {
+    // 浏览器刷新
+    reload() {
+      location.reload()
+    }
   }
 }
 </script>
